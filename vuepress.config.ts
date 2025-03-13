39e4aa1eebb3d077ec32f8c5db7e3d2ae1bfbe8a
xmz-Ruby <57243940+xmz-Ruby@users.noreply.github.com>
2025-03-13 10:47:06 +0800

    update CherryHQ

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -58,6 +58,14 @@ export default defineUserConfig({
       // 如果你使用的不是 cloudflare Pages 部署需要删掉这一行，因为如果不是cloudflare Pages部署，这个代理是无法正常工作的
       downProxy: cloudflarePagesDownProxy(),
     },
+    {
+      mountPath: "/常用工具/人工智能/cherry-studio",
+      analysis: githubReleasesFilesAnalysis({ user: "CherryHQ", repository: "cherry-studio" }),
+      // 下载代理配置,支持多个平台，参考:https://jjaw.cn/2024/8/3/flist-config-porxy/
+      // 这个是为了解决github的国内下载慢的问题，和跨域问题，建议配置，不然pdf，txt，md等文件因为跨域无法预览
+      // 如果你使用的不是 cloudflare Pages 部署需要删掉这一行，因为如果不是cloudflare Pages部署，这个代理是无法正常工作的
+      downProxy: cloudflarePagesDownProxy(),
+    },
     // {
     //   mountPath: "/",
     //   // 这里使用 fileUrlTreeAnalysis 文件放到对应的文件路径中
