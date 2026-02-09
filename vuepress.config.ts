28df211e53d027ec7e5821228d9765c18699bf5f
xmz-Ruby <57243940+xmz-Ruby@users.noreply.github.com>
2026-02-09 09:48:41 +0800

    add

--- vuepress.config.ts
+++ vuepress.config.ts
@@ -157,10 +157,10 @@ export default defineUserConfig({
     //   downProxy: cloudflarePagesDownProxy(),//如果文件树地址下载比较慢，也可以配置代理
     // },
     {
-      mountPath: "/tvbox",
+      mountPath: "/娱乐软件/影视",
       // 这里使用 fileUrlTreeAnalysis 文件放到对应的文件路径中
       analysis: fileUrlTreeAnalysis({
-        "/让我看看-电视版64位-1.1.0.apk": "https://15414.kstore.space/%E8%AE%A9%E6%88%91%E7%9C%8B%E7%9C%8B-%E7%94%B5%E8%A7%86%E7%89%8864-1.1.0.apk",
+        "/让我看看-电视版64位-1.1.0.apk": "https://15414.kstore.space/%E8%AE%A9%E6%88%91%E7%9C%8B%E7%9C%8B-%E7%94%B5%E8%A7%86%E7%89%8864%E4%BD%8D-1.1.0.apk",
         "/让我看看-电视版-1.1.0.apk": "https://15414.kstore.space/%E8%AE%A9%E6%88%91%E7%9C%8B%E7%9C%8B-%E7%94%B5%E8%A7%86%E7%89%88-1.1.0.apk",
         "/让我看看-手机版-1.1.0.apk": "https://15414.kstore.space/%E8%AE%A9%E6%88%91%E7%9C%8B%E7%9C%8B-%E6%89%8B%E6%9C%BA%E7%89%88-1.1.0.apk"
       }),
